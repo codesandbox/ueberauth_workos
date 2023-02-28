8341d222a04010ca3496b54a78cd1cd3b3d38277
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2023-02-28 11:03:11 -0500

    Install Ueberauth and begin strategy

---
+++ lib/ueberauth/strategy/workos.ex
@@ -0,0 +1,13 @@
+defmodule Ueberauth.Strategy.WorkOS do
+  @moduledoc """
+  Implementation of an Ueberauth Strategy for WorkOS Single Sign-On
+
+  ## Configuration
+
+  This provider supports the following configuration:
+
+    * TODO
+
+  """
+  use Ueberauth.Strategy
+end
